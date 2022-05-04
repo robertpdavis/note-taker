4a4fe7f769ec316ed1b688b09412875ff042a3d8
Buxta <robertpdavis@optusnet.com.au>
2022-05-04 23:16:18 +1000

    Server.js file created and basic client server route functionality created.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const { clog } = require('./middleware/clog');
+const { readAndAppend, readFromFile } = require('./helpers/fsUtils');
 
+//Use env. port (for remote server) or if not available 3001.
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -17,40 +20,47 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 //As only a few routes related to notes category, no seperated route handlers required
-// GET Route for notes page
+// GET Route for notes html page
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET API route to retrieve the saved notes as JSON.
+app.get('/api/notes', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+
 // POST Route for submitting a note
-fb.post('/', (req, res) => {
+app.post('/api/notes', (req, res) => {
     // Destructuring assignment for the items in req.body
-    const { email, feedbackType, feedback } = req.body;
+    const { title, text } = req.body;
+    const noteId = uuidv4();
 
     // If all the required properties are present
-    if (email && feedbackType && feedback) {
+    if (title && text) {
         // Variable for the object we will save
-        const newFeedback = {
-            email,
-            feedbackType,
-            feedback,
-            feedback_id: uuidv4(),
+        const newNote = {
+            created: Date.now(),
+            noteId,
+            title,
+            text,
         };
 
-        readAndAppend(newFeedback, './db/db.json');
+        readAndAppend(newNote, './db/db.json');
 
         const response = {
             status: 'success',
-            body: newFeedback,
+            body: newNote,
         };
 
         res.json(response);
     } else {
-        res.json('Error in posting feedback');
+        res.json('Error in posting note');
     }
 });
 
-// Wildcard route to direct all other get requests to the home page
+// Wildcard route to direct all other get requests to the home html page
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
